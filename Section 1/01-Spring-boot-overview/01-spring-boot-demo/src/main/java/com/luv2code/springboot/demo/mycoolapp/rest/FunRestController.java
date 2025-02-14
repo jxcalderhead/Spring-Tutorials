0232e1ff87d950e45b7861c845d354c1d3ca1c8d
jxcalderhead <jxcalderhead1@malone.edu>
2025-02-14 17:07:21 -0500

    Reorganizing

--- Section 1/01-Spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
+++ Section 1/01-Spring-boot-overview/01-spring-boot-demo/src/main/java/com/luv2code/springboot/demo/mycoolapp/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.luv2code.springboot.demo.mycoolapp.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,4 +13,9 @@ public class FunRestController {
     public String sayHello() {
         return "Hello World!";
     }
+
+    @Value("${coach.name}")
+    private String coachName;
+
+
 }
